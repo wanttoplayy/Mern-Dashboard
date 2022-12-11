b4c07787a0a4269f08027ec7e7d092986a953c68
Vasin <bestwasin1617@gmail.com>
2022-12-11 13:59:00 +0700

    feat:add client/server

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { LightModeOutlined, DarkModeOutlined, Menu as MenuIcon, Search, SettingsOutlined, ArrowDropDownOutlined } from '@mui/icons-material'
+import FlexBetween from './FlexBetween'
+import { useDispatch } from 'react-redux'
+import { setMode } from "state"
+import profileImage from "assets/Profiles.jpg"
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
